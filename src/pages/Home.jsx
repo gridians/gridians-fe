0d57f61c65d47d3f557cc95a6f2eadc79d9713e3
eunyechan <glqgkq384@naver.com>
2023-01-20 19:30:29 +0900

    #1 feat: comment 화면 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import Comment from "../components/comment/Comment";
 
 export default function Home() {
-  return (
-    <HomeContainer>
-      <Comment />
-    </HomeContainer>
-  );
+  return <HomeContainer></HomeContainer>;
 }
 
 const HomeContainer = styled.div`
