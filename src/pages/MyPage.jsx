c3081c2770ac1896480adc25e1a228acd9a5c450
eunyechan <glqgkq384@naver.com>
2023-01-24 16:04:23 +0900

    #8 feat: mypage 레이아웃 완료

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { AiOutlineIdcard } from "react-icons/ai";
