c264e2c395d4711e6388c9ae4c31e3541e787581
taewok <taewok5163@naver.com>
2023-01-31 23:15:29 +0900

    #20 feat: 카드 정보 관련 recoil 생성

---
+++ src/store/cardInfoAtom.js
@@ -0,0 +1,39 @@
+import { atom } from "recoil";
+import { v1 } from "uuid";
+
+export const imgSrc = atom({
+  key: `imgSrc/${v1()}`,
+  default: "",
+});
+export const statusMessage = atom({
+  key: `statusMessage/${v1()}`,
+  default: "",
+});
+export const position = atom({
+  key: `position/${v1()}`,
+  default: "",
+});
+export const language = atom({
+  key: `language/${v1()}`,
+  default: [],
+});
+export const introduceText = atom({
+  key: `introduceText/${v1()}`,
+  default: "",
+});
+export const github = atom({
+  key: `github/${v1()}`,
+  default: "",
+});
+export const instagram = atom({
+  key: `instagram/${v1()}`,
+  default: "",
+});
+export const twitter = atom({
+  key: `twitter/${v1()}`,
+  default: "",
+});
+export const tag = atom({
+  key: `tag/${v1()}`,
+  default: [],
+});
