39946a7118c2f8bb67607ded0133dcf10f33c1bd
taewok <taewok5163@naver.com>
2023-02-23 15:51:50 +0900

    #30 feat: 카드 등록 post500 오류 해결

--- src/apis/queries/cardEnrollQuery.js
+++ src/apis/queries/cardEnrollQuery.js
@@ -4,12 +4,18 @@ import { api2 } from "../untils";
 // 로그인한 유저 카드 등록
 export const cardEnrollUseMutationPostToken = () => {
   console.log("카드 등록");
-  const res = api2.post(`/cards`, {
-    headers: {
-      "Content-Type": "application/json",
-      accept: "application/json,",
-      Authorization: `Bearer ${getCookieToken("accessToken")}`,
-    },
-  });
+  console.log(getCookieToken("accessToken"));
+  const res = api2.post(
+    `/cards`,
+    //파라미터 전달이 없어도 {}를 써야 headers전달이 가능
+    {},
+    {
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json,",
+        Authorization: `Bearer ${getCookieToken("accessToken")}`,
+      },
+    }
+  );
   return res.data;
 };
