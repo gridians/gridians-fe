86483c44390a50b348bf75f5a84ab0a67ee062ab
taewok <taewok5163@naver.com>
2023-02-23 22:20:09 +0900

    #39 feat: 깃헙 등록 기능

--- src/pages/GithubLoginPage.jsx
+++ src/pages/GithubLoginPage.jsx
@@ -1,31 +1,34 @@
-import axios from "axios";
 import React, { useEffect } from "react";
 import { useMutation } from "react-query";
-import { useLocation,useNavigate } from "react-router";
-import { useRecoilState } from "recoil";
+import { useLocation, useNavigate } from "react-router";
+import { useSetRecoilState } from "recoil";
+import { loginUseMutationPostToken } from "../apis/queries/loginQuery";
 import { loginGithubId } from "../store/userInfoAtom";
 
 const GithubLoginPage = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
-  const [githubId,setGithubId] = useRecoilState(loginGithubId);
+  const setGithubId = useSetRecoilState(loginGithubId);
+
+  const { mutate: githubSendCode } = useMutation(
+    "githubCode",
+    (code) => loginUseMutationPostToken(code),
+    {
+      onSuccess: (res) => {
+        console.log(res);
+        navigate("/memberlistpage");
+      },
+      onError: (err) => {
+        console.log(err);
+      },
+    }
+  );
 
   useEffect(() => {
     const code = location.search.split("=")[1];
-    console.log(code);
-    axios.post(`http://152.70.251.225:8080/user/auth/social-login`, {
-      token: code
-    }).then((data)=>{
-      console.log("ok",data.data);
-    }).catch((err)=>{
-      console.log(err.response.data.param);
-      if(err.response.data.message === "Git id not found"){
-        setGithubId(err.response.data.param);
-        navigate("/signup");
-      }
-    })
-  }, [location.hash, location.search, navigate, setGithubId]);
+    githubSendCode(code);
+  }, []);
   return <div></div>;
 };
 
