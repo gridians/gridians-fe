b23b075f7cb959a37bc163a1f0c7d5742982b86d
taewok <taewok5163@naver.com>
2023-02-28 19:11:52 +0900

    feat: font 변경

--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -1,6 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'SUIT-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-Regular.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
   &::-webkit-scrollbar {
     width: 10px;
     background-color: #0E0909;
@@ -15,6 +21,7 @@ const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
+    font-family:'SUIT-Regular' ;
     #root{
         /* width: 100%;
         height: 100%; */
